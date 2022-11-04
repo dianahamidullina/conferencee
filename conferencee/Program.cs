f9494b4c6e4f2a124c736c520b0da2f02c0f974b
Diana <Diana@DIDIDSHAM>
2022-11-04 20:18:24 +0300

    Добавьте файлы проекта.

---
+++ conferencee/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conferencee
+{
+    public class Boris { }
+    class Rashid : Boris { } //finance director
+    class Lukas : Rashid { }
+    class Buh : Lukas { }
+    class OIlham : Boris { } // automatisation director
+    class Orcadiy : OIlham { }
+    class Volodya : Orcadiy { }
+    class Ilshat : Volodya { } // system
+    class Ivanich : Ilshat { }
+    class Ilya : Ivanich { }
+    class Vitya : Ivanich { }
+    class Jeka : Ivanich { }
+    class Sergey : Volodya { }// development
+    class Lyasan : Sergey { }
+    class Marat : Lyasan { }
+    class Dina : Lyasan { }
+    class Ildar : Lyasan { }
+    class Anton : Lyasan { }
+    internal class Program
+
+    {
+        static void spisok()
+        {
+            string path = (@"C:\Users\Diana\source\repos\conferencee\conferencee\TextFile1.txt");
+
+            string text = File.ReadAllText(path);
+            Console.WriteLine($"{text}");
+        }
+        static void Otvet(Type a, Type b)
+        {
+            if (a.IsSubclassOf(b)) { Console.WriteLine("Yes"); } // проверяем является ли классы наследниками друг друга
+            else { Console.WriteLine("No"); }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Озакомьтесь с сотрудниками офиса:");
+            spisok();
+            Console.WriteLine("Введите от кого хотите дать задачу (на английском)");
+            string cto= Console.ReadLine();
+            Console.WriteLine("Введите кому хотите дать задачу (на английском)");
+            string comy = Console.ReadLine();
+            Console.WriteLine("Введите название задачи");
+            string what = Console.ReadLine();
+            Type C = Type.GetType("conferencee" + "." + cto, false, true); // получаем типы классов по имени(строке)
+            Type C2 = Type.GetType("conferencee" + "." + comy, false, true);
+            Console.WriteLine($"Итак, от {cto} дана задача {what}  для {comy}.");
+            Console.WriteLine("Ответ сотрудника:");
+            Otvet(C2, C);
+        }
+    }
+}
---
+++ tumakov.8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("tumakov.8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tumakov.8")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7ec4b687-0c58-4f41-83b1-139df345b962")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
